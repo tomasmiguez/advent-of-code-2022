47f8d77c43b46c4e30cf79008bf32436ef553b03
Tomas Miguez <tmiguez@fu.do>
2022-12-10 10:57:29 -0300

    feat: completed day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -11,37 +11,54 @@ use crate::Playable::*;
 
 fn letter_to_playable(letter: char) -> Playable {
     match letter {
-        'A' | 'X' => Rock,
-        'B' | 'Y' => Paper,
-        'C' | 'Z' => Scissors,
-        _         => panic!("Invalid playable char!"),
+        'A' => Rock,
+        'B' => Paper,
+        'C' => Scissors,
+        _   => panic!("Invalid playable char!"),
+    }
+}
+
+enum Result {
+    Lose,
+    Draw,
+    Win,
+}
+
+use crate::Result::*;
+
+fn letter_to_wanted_result(letter: char) -> Result {
+    match letter {
+        'X' => Lose,
+        'Y' => Draw,
+        'Z' => Win,
+        _   => panic!("Invalid result char!"),
     }
 }
 
 fn match_score(r#match: String) -> i32 {
     let oponent_play = letter_to_playable(r#match.chars().nth(0).unwrap());
-    let player_play = letter_to_playable(r#match.chars().nth(2).unwrap());
+    let player_play = letter_to_wanted_result(r#match.chars().nth(2).unwrap());
 
     match oponent_play {
         Rock => {
             match player_play {
-                Rock => 3 + 1,
-                Paper => 6 + 2,
-                Scissors => 0 + 3,
+                Lose => 0 + 3,
+                Draw => 3 + 1,
+                Win => 6 + 2,
             }
         },
         Paper => {
             match player_play {
-                Rock => 0 + 1,
-                Paper => 3 + 2,
-                Scissors => 6 + 3,
+                Lose => 0 + 1,
+                Draw => 3 + 2,
+                Win => 6 + 3,
             }
         },
         Scissors => {
             match player_play {
-                Rock => 6 + 1,
-                Paper => 0 + 2,
-                Scissors => 3 + 3,
+                Lose => 0 + 2,
+                Draw => 3 + 3,
+                Win => 6 + 1,
             }
         }
     }
